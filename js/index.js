89c4f2c42878d684092d84c8fabf87d39978c4b5
devayc <devayc3737@gmail.com>
2022-04-12 17:40:43 +0300

    calendary event ready

--- js/index.js
+++ js/index.js
@@ -1,35 +1,17 @@
-<!-- VK Widget 
-<div id="vk_groups"></div>
-<script type="text/javascript">
-VK.Widgets.Group("vk_groups", {mode: 4, no_cover: 1, width: "220", height: "540", color1: '912728', color2: 'FFFE03', color3: 'FFFE03'}, 182417670);
-</script>
--->
-<!--
-<script type="text/javascript" src="https://vk.com/js/api/openapi.js?162"></script>
--->
-<!-- VK Widget 
-<div id="vk_community_messages"></div>
-<script type="text/javascript">
-VK.Widgets.CommunityMessages("vk_community_messages", 182417670, {tooltipButtonText: "Есть вопрос?"});
-</script>
--->
- <!--Facebook Pixel Code  
-<script> 
-<! --!function(f,b,e,v,n,t,s) 
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod? 
-n.callMethod.apply(n,arguments):n.queue.push(arguments)}; 
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; 
-n.queue=[];t=b.createElement(e);t.async=!0; 
-t.src=v;s=b.getElementsByTagName(e)[0]; 
-s.parentNode.insertBefore(t,s)}(window, document,'script', 
-'https://connect.facebook.net/en_US/fbevents.js'); 
-fbq('init', '2611773998907393'); 
-fbq('track', 'PageView'); 
-</script> 
-<noscript><img height="1" width="1" style="display:none" 
-src="https://www.facebook.com/tr?id=2611773998907393&ev=PageVi.." 
-/></noscript> -->
--->
-<!-- End Facebook Pixel Code -->
-<!--
-<script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?162",t.onload=function(){VK.Retargeting.Init("VK-RTRG-437617-9hkQW"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-437617-9hkQW" style="position:fixed; left:-999px;" alt=""/></noscript> -->
+
+  new AirDatepicker('#start', {
+  multipleDatesSeparator: ' - ',
+    range: true,
+    autoClose: false,
+    inline: true,
+  onSelect({ formattedDate }) {
+    $("#start").val(formattedDate[0].split("-"));
+    if (formattedDate.length == 2) {
+      $("#end").val(formattedDate[1].split(","));
+    }
+      
+        
+  },
+  
+})
+
